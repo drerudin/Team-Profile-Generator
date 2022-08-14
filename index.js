e4c4066c8f44d229ec5b2923ab84edba3e2c850b
drerudin <drerudin@gmail.com>
2022-08-13 19:58:57 -0700

    function generated for inquirer

--- index.js
+++ index.js
@@ -1,6 +1,14 @@
 const fs = require('fs')
 const inquirer = require('inquirer');
+const Employee = require('../lib/Employee')
+const Engineer = require("../lib/Engineer");
+const Intern = require("../lib/Intern");
+const Manager = require("../lib/Manager")
+import generateTeam from ('./dist/page-template')
 
+
+
+function createManager() {
 inquirer
     .prompt({
         type: 'list',
@@ -58,5 +66,7 @@ inquirer
         idArray.push(answers.managerId)
         createTeam()
     })
+}
 
+createManager(generateTeam);
 
